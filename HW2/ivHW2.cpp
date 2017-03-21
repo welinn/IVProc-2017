97d22f4a3283bf911acb8a3620b69fd527697d9b
welinn <susutruelove@gmail.com>
2017-03-21 09:10:07 +0800

    Update ivHW2.cpp

--- HW2/ivHW2.cpp
+++ HW2/ivHW2.cpp
@@ -6,8 +6,28 @@ using namespace cv;
 Mat src;
 Rect rect;
 
-void onMouse(int event, int x, int y, int flags, void *param)
-{  
+void onMouse(int, int, int, int, void);
+void saveSample();
+
+
+int main(){
+	
+//  int count
+  src=imread("./hw2.jpg");
+  if(src.data == 0){
+    printf("no image\n");
+    return -1;
+  }
+  namedWindow("Image");
+  imshow("Image",src);
+  setMouseCallback("Image", onMouse, NULL);
+  //waitKey();
+  
+  cvDestroyWindow("Image");
+  return 0;
+}
+
+void onMouse(int event, int x, int y, int flags, void *param){
   Mat img = src.clone();
   switch (event){ 
     //滑鼠左鍵
@@ -29,16 +49,6 @@ void onMouse(int event, int x, int y, int flags, void *param)
   }  
 }  
 
-int main()
-{
-  src=imread("./hw2.jpg");
-  if(src.data == 0){
-      printf("no image\n");
-      return -1;
-  }
-  namedWindow("Image");
-  imshow("Image",src);
-  setMouseCallback("Image", onMouse, NULL);
-  waitKey();
-  return 0;
+void saveSample(){
 }
+
